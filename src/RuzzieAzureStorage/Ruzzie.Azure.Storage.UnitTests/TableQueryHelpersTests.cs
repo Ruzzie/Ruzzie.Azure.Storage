c5aabb6f4e3218acade607c0292e835fb041b6c4
Ruzzie <dorus.verhoeckx@gmail.com>
2020-02-14 12:48:44 +0100

    [fix] unsaved file

--- src/RuzzieAzureStorage/Ruzzie.Azure.Storage.UnitTests/TableQueryHelpersTests.cs
+++ src/RuzzieAzureStorage/Ruzzie.Azure.Storage.UnitTests/TableQueryHelpersTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using FluentAssertions;
+﻿using FluentAssertions;
 using FsCheck.Xunit;
 
 namespace Ruzzie.Azure.Storage.UnitTests
@@ -10,28 +9,6 @@ namespace Ruzzie.Azure.Storage.UnitTests
         public void CreatePointQueryFilterForPartitionAndRowKey_PropertyTests(string partitionKey, string rowKey)
         {
            TableQueryHelpers.CreatePointQueryFilterForPartitionAndRowKey(partitionKey, rowKey).Should().NotBeNull();
-        }
-    }
-
-
-    public class KeyHelperTests
-    {
-        [Property]
-        public void CreateKeyForDateTimeWithPaddingAndReverseOrder_PropertyTests(DateTime dateTime)
-        {
-            dateTime.CreateKeyForDateTimeWithPaddingAndReverseOrder().Should().NotBeNullOrWhiteSpace();
-        }
-
-        [Property]
-        public void EncodeInvalidKeyChars_PropertyTests(string input)
-        {
-            input.EncodeInvalidKeyChars().Should().NotContain("/");
-        }
-
-        [Property]
-        public void DecodeInvalidKeyChars_PropertyTests(string input)
-        {
-            input.DecodeInvalidKeyChars().Should().NotContain("_");
         }
     }
 }
