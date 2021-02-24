a375291071167858cd5e2bf75320e3b029a02d5f
Ruzzie <dorus.verhoeckx@gmail.com>
2021-02-24 08:41:53 +0100

    upgraded to use latest version of Ruzzie.Common Refactored the CloudTable Pool (with bw compat): with shorthand execute functions, to make use of a SingleObjectPool for usage with the new sdk version (since the new version is thread safe), defines interfaces to make usage easiers and easier upgrade path. upgraded to latest minor version of Azure.Cosmos.Table sdk

--- src/RuzzieAzureStorage/Ruzzie.Azure.Storage/CloudTablePool.cs
+++ src/RuzzieAzureStorage/Ruzzie.Azure.Storage/CloudTablePool.cs
@@ -1,24 +1,38 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.Azure.Cosmos.Table;
 using Ruzzie.Common.Threading;
 
 namespace Ruzzie.Azure.Storage
 {
+    public interface ITablePool<T>
+    {
+        string                  TableName { get; }
+        IObjectPool<T> Pool      { get; }
+
+        /// Executed the given function in the configured pool <see cref="IObjectPool{T}.ExecuteOnAvailableObject{TResult}"/>, for easy access.
+        public TResult Execute<TResult>(Func<T, TResult> funcToExecute);
+
+        /// Executed the given function async in the configured pool <see cref="IObjectPool{T}.ExecuteOnAvailableObject{TResult}"/>, for easy access.
+        public Task<TResult> ExecuteAsync<TResult>(Func<T, Task<TResult>> funcToExecute);
+    }
+
     /// <summary>
     /// Pool of CloudTables. If the table does not exist, it will automatically be created.
     /// </summary>
-    public class CloudTablePool
+    public class CloudTablePool : ITablePool<CloudTable>
     {
         public string TableName { get; }
         private readonly Task _createTableIfNotExistsTask;
-        private readonly ThreadSafeObjectPool<CloudTable> _pool;
+        private readonly IObjectPool<CloudTable> _pool;
 
         /// <summary>
-        /// Create a new Pool of CloudTables. If the table does not exist, it will automatically be created.
+        /// Create a new ThreadSafe Pool of CloudTables. If the table does not exist, it will automatically be created.
         /// </summary>
         /// <param name="tableName"></param>
         /// <param name="cloudStorageAccount"></param>
         /// <param name="poolSize">Size of the pool.</param>
+        [Obsolete("the new azure table client sdk version is thread safe, this ctor is obsolete.")]
         public CloudTablePool(string tableName, CloudStorageAccount cloudStorageAccount, int poolSize = 16)
         {
             TableName = tableName;
@@ -35,7 +49,43 @@ namespace Ruzzie.Azure.Storage
             }
         }
 
-        public ThreadSafeObjectPool<CloudTable> Pool
+        /// <summary>
+        /// Create access to CloudTables. If the table does not exist, it will automatically be created.
+        /// </summary>
+        /// <param name="tableName"></param>
+        /// <param name="cloudTableClient"></param>
+        /// <remarks>As of the new azure.cosmos.table client sdk v2, pooling and locking isn't necessary anymore. So we provide easy access to tables and reuse a single instance.
+        /// https://github.com/Azure/azure-cosmos-dotnet-v2/issues/629
+        /// </remarks>
+        public CloudTablePool(string tableName, CloudTableClient cloudTableClient)
+        {
+            TableName                   = tableName;
+            _pool                       = new SingleObjectPool<CloudTable>(GetNewTableReference());
+            _createTableIfNotExistsTask = Pool.ExecuteOnAvailableObject(table => table.CreateIfNotExistsAsync());
+            _createTableIfNotExistsTask.ContinueWith(task =>
+            {
+                _createTableIfNotExistsTask.Dispose();
+            });
+
+            CloudTable GetNewTableReference()
+            {
+                return cloudTableClient.GetTableReference(tableName);
+            }
+        }
+
+        /// Executed the given function in the configured pool <see cref="IObjectPool{T}.ExecuteOnAvailableObject{TResult}"/>, for easy access.
+        public TResult Execute<TResult>(Func<CloudTable,TResult> funcToExecute)
+        {
+            return Pool.ExecuteOnAvailableObject(funcToExecute);
+        }
+
+        /// Executed the given function async in the configured pool <see cref="IObjectPool{T}.ExecuteOnAvailableObject{TResult}"/>, for easy access.
+        public async Task<TResult> ExecuteAsync<TResult>(Func<CloudTable, Task<TResult>> funcToExecute)
+        {
+            return await Pool.ExecuteOnAvailableObject(funcToExecute);
+        }
+
+        public IObjectPool<CloudTable> Pool
         {
             get
             {
